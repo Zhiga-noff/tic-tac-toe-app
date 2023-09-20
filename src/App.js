825371c139aa20290bb5b388560fc10502541d09
Kirill Zhiganov <zhigano_k.n@mail.ru>
2023-09-20 16:31:03 +0300

    Переделал на классовый компонент компонент StructuralComponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import style from './App.module.css';
-import { StructuralComponent } from './components/StructuralComponent';
+import {  StructuralComponentClass } from './components/StructuralComponent';
 import { isWin, getTypeField } from './modules';
 import { store } from './store/store';
 import { connect, useDispatch, useSelector } from 'react-redux';
@@ -7,43 +7,6 @@ import { selectType, selectArray, selectReset } from './store/selectors';
 import { REVERSE_FLAG, RESET_GAME } from './store/actions';
 import { Component } from 'react';
 
-export const App = () => {
-  // Установка типов круг или крест
-  const typeField = useSelector(selectType);
-  // Пустой массив для внесения типов элементов на поле
-  const arrClickResult = useSelector(selectArray);
-  // Флаг для сброса полей
-  const reset = useSelector(selectReset);
-
-  const dispatch = useDispatch();
-
-  return (
-    <>
-      <div className={style.flexo}>
-        <div className={style.result}>
-          {isWin(arrClickResult) ? (
-            <p key={'endGame'}>Игра окончена</p>
-          ) : (
-            [<p key={'message'}>Ваш ход</p>, getTypeField(typeField)]
-          )}
-        </div>
-        <div
-          className={style.reset}
-          onClick={() => {
-            dispatch(REVERSE_FLAG);
-            dispatch(RESET_GAME);
-          }}
-        >
-          {reset ? 'Play' : 'Reset'}
-        </div>
-      </div>
-
-      <div className={style.app}>
-        <StructuralComponent />
-      </div>
-    </>
-  );
-};
 
 export class AppClassContainer extends Component {
     constructor(props) {
@@ -74,7 +37,7 @@ export class AppClassContainer extends Component {
                 </div>
 
                 <div className={style.app}>
-                    <StructuralComponent />
+                    <StructuralComponentClass />
                 </div>
             </>
         );
--- src/components/StructuralComponent.js
+++ src/components/StructuralComponent.js
@@ -1,7 +1,8 @@
 import { ClickInField } from './Stateful/ClickInField';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import { selectArray } from '../store/selectors';
+import { Component } from 'react';
 
 export const StructuralComponent = () => {
   const arrClickResult = useSelector(selectArray);
@@ -15,8 +16,26 @@ export const StructuralComponent = () => {
   );
 };
 
-StructuralComponent.propTypes = {
-  typeField: PropTypes.string,
-  setTypeField: PropTypes.func,
-  isWin: PropTypes.func,
+export class StructuralComponentClassContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+                {this.props.arrClickResult.map((item, index) => (
+                    <ClickInField key={index} dataIndex={index} />
+                ))}
+            </>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        arrClickResult: state.arrayState,
+    };
 };
+
+export const StructuralComponentClass = connect(mapStateToProps)(StructuralComponentClassContainer)
