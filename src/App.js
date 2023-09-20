51868b34596882605e93b01b4cd122ede4a8d0d4
Kirill Zhiganov <zhigano_k.n@mail.ru>
2023-09-20 16:26:24 +0300

    Переделал на классовый компонент компонент App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import style from './App.module.css';
 import { StructuralComponent } from './components/StructuralComponent';
-import PropTypes from 'prop-types';
 import { isWin, getTypeField } from './modules';
 import { store } from './store/store';
-import { useDispatch, useSelector } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import { selectType, selectArray, selectReset } from './store/selectors';
 import { REVERSE_FLAG, RESET_GAME } from './store/actions';
+import { Component } from 'react';
 
 export const App = () => {
   // Установка типов круг или крест
@@ -45,11 +45,56 @@ export const App = () => {
   );
 };
 
-App.propTypes = {
-  typeField: PropTypes.string,
-  setTypeField: PropTypes.func,
-  arrClickResult: PropTypes.array,
-  setArrClickResult: PropTypes.func,
-  getTypeField: PropTypes.func,
-  isWin: PropTypes.func,
+export class AppClassContainer extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <>
+                <div className={style.flexo}>
+                    <div className={style.result}>
+                        {isWin(this.props.arrClickResult) ? (
+                            <p key={'endGame'}>Игра окончена</p>
+                        ) : (
+                            [<p key={'message'}>Ваш ход</p>, getTypeField(this.props.typeField)]
+                        )}
+                    </div>
+                    <div
+                        className={style.reset}
+                        onClick={() => {
+                            this.props.dispatch(REVERSE_FLAG);
+                            this.props.dispatch(RESET_GAME);
+                        }}
+                    >
+                        {this.props.reset ? 'Play' : 'Reset'}
+                    </div>
+                </div>
+
+                <div className={style.app}>
+                    <StructuralComponent />
+                </div>
+            </>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        reset: state.resetState,
+        arrClickResult: state.arrayState,
+        typeField: state.typeState
+    };
 };
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onAgeIncrease: () => dispatch(),
+        onAgeReset: () => dispatch(),
+    };
+};
+
+export const AppClass=connect(mapStateToProps)(AppClassContainer)
+
